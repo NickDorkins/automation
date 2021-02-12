3f1207e04b3df34ea5b8d32ed333d09fe4321e54
NickDorkins <nick.dorkins@gmail.com>
2021-02-11 22:54:32 -0800

    partial

--- automation/automation.py
+++ automation/automation.py
@@ -1,10 +1,19 @@
 import re
 import shutil
 
+#  Clean up the Phone numbers so they all match the same format
+# Format: ###-###-####
+
+def normalize_phone_numbers(phone_number):
+    
+
+
 #  Find all phone numbers in the given document
 
-
+def phone_finder():
 #  Validate the phone numbers against existing documents
+
+# Store phone numbers in a new file
 
 
 # Find all email addresses in given document
@@ -13,6 +22,6 @@ import shutil
 # Validate the email addresses against existing documents
 
 
-
+# Store Emails in a new file
 
 
