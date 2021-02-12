760d1aa4573eb4cb86f0b0e2bf91c36f2e0c3e96
NickDorkins <nick.dorkins@gmail.com>
2021-02-11 22:32:06 -0800

    First Commit

---
+++ automation/automation.py
@@ -0,0 +1,18 @@
+import re
+import shutil
+
+#  Find all phone numbers in the given document
+
+
+#  Validate the phone numbers against existing documents
+
+
+# Find all email addresses in given document
+
+
+# Validate the email addresses against existing documents
+
+
+
+
+
